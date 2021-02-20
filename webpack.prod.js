269004a048e9cb8a23ec6823ad02ec96b4f5929f
limengyi <1051933474@qq.com>
2021-02-20 15:48:10 +0800

    chore: 调整静态文件的打包生成目录，不采取相对路径读取的方式，防止与页面导航冲突

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,1 +1,17 @@
-module.exports = require('webpack-project-builder/lib/webpack.prod')
+const { merge } = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const prodConfig = require('webpack-project-builder/lib/webpack.prod');
+
+let prodSetConfig = {
+  output: {
+    filename: 'static/[name]_[chunkhash:8].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'static/[name]_[contenthash:8].css',
+    }),
+  ]
+}
+
+module.exports = merge(prodConfig, prodSetConfig);
--- webpack.ssr.js
+++ webpack.ssr.js
@@ -1,1 +1,18 @@
-module.exports = require('webpack-project-builder/lib/webpack.ssr');
+const { merge } = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ssrConfig = require('webpack-project-builder/lib/webpack.ssr');
+
+let ssrSetConfig = {
+  output: {
+		filename: 'static/[name]-server.js',
+		libraryTarget: 'umd',
+		publicPath: '/'
+	},
+  plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'static/[name]_[contenthash:8].css'
+		})
+	]
+}
+
+module.exports = merge(ssrConfig, ssrSetConfig);
